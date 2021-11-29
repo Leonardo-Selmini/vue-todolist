90163a097bcc6d17490334488376e218579d427a
Leonardo.Selmini <selminileonardo@gmail.com>
2021-11-29 18:06:39 +0100

    milestone 1

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
 const app = new Vue({
   el: '#root',
   data: {
+    inputText: '',
+    list: [
+      {
+        text: 'fare la spesa',
+        notDone: false
+      },
+    ]
 
   },
   methods: {
-    
-  }
+    removeLi: function (index) {
+      this.list.splice(index, 1)
+    },
+    addLi: function () {
+      if (this.inputText != '') {
+        const newLi = {
+          text: this.inputText,
+          notDone: false
+        }
+        this.list.push(newLi);
+        this.inputText = '';
+      } else {
+        alert('Non hai inserito una commissione')
+      }
+    }
+  },
 })
